cb077b2f4caf857dadcc195a082e641715e66abb
dbdealer <dbdealer@users.noreply.github.com>
2020-09-21 12:54:42 -0300

    Adicionar arquivos de projeto.

---
+++ DFO_QATest_UserAPI/Controllers/UsersController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DFO_QATest_UserAPI.Models;
+
+namespace DFO_QATest_UserAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserContext _context;
+
+        public UsersController(UserContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Users>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Users>> GetUserItem(long id)
+        {
+            var userItem = await _context.Users.FindAsync(id);
+
+            if (userItem == null)
+            {
+                return NotFound();
+            }
+
+            return userItem;
+        }
+
+        // PUT: api/Users/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUserItem(long id, Users userItem)
+        {
+            if (id != userItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(userItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Users
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Users>> PostUserItem(Users userItem)
+        {
+            _context.Users.Add(userItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUserItem), new { id = userItem.Id }, userItem);
+        }
+
+        // DELETE: api/Users/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Users>> DeleteUserItem(long id)
+        {
+            var userItem = await _context.Users.FindAsync(id);
+            if (userItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(userItem);
+            await _context.SaveChangesAsync();
+
+            return userItem;
+        }
+
+        private bool UserItemExists(long id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ DFO_QATest_UserAPI/Models/Users.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DFO_QATest_UserAPI.Models
+{
+    public class Users
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(12, 150)]
+        public int Age { get; set; }
+        
+        [StringLength(50)] 
+        public string Address { get; set; }
+    }
+}
